2938d0d9ef6f2462731348f58cb4f2f1ac2944a8
jennifermthomsen <91202309+jennifermthomsen@users.noreply.github.com>
2022-11-16 18:33:54 +0100

    css changes

--- mul-bookings-main/src/App.jsx
+++ mul-bookings-main/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import './index.css';
 import './components/Header.css';
+import './components/Buttons.css';
 import MainContent from "./components/MainContent";
 import TheHeader from "./components/TheHeader";
 import BookingButton from "./components/BookingButton";
