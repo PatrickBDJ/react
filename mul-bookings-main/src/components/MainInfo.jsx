cc35fde5c4275dadc1952dca1c3eb2e08898ffea
jennifermthomsen <91202309+jennifermthomsen@users.noreply.github.com>
2022-11-17 17:52:41 +0100

    css

--- mul-bookings-main/src/components/MainInfo.jsx
+++ mul-bookings-main/src/components/MainInfo.jsx
@@ -7,7 +7,7 @@ function MainInfo() {
         <div className="App-div">
             <div className="overview">
                 <h1>Min oversigt</h1>
-                <a href=""><img src={info} alt="Info" id="info-styling" /></a>
+                <img src={(info)} alt="Info" id="info-styling" />
             </div>
         </div>
     );
