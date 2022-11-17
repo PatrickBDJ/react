f7ba4af33017fda58d8935b35758445d81a271b2
jennifermthomsen <91202309+jennifermthomsen@users.noreply.github.com>
2022-11-17 18:59:48 +0100

    css

--- mul-bookings-main/src/components/BookingForm.jsx
+++ mul-bookings-main/src/components/BookingForm.jsx
@@ -21,48 +21,49 @@ export default function BookingForm() {
   };
 
   return (
-  <div className="form-div">
-    <h2>Foretag ny booking</h2>
-    <br />
-    <form id="booking-modal">
-      <div>
-      <label class="label-title-styling">Lokaletype:</label>
-        <button id="active-roomtype">Undervisningslokaler</button>
-        <button id="inactive-roomtype">Mødelokaler</button>
-      </div>
+    <div className="form-div">
+      <h2>Foretag ny booking</h2>
       <br />
-      <div>
-        <label class="label-title-styling">Lokale:</label>
-        <input class="input-styling"
-          type="text"
-          placeholder="Lokale nummer:"
-          name="room"
-          value={room}
-          onChange={handleRoom}
-        />
-      </div>
-      <br />
-      <div>
-        <label class="label-title-styling">Dato:</label>
-        <input class="input-styling"
-          type="text"
-          placeholder="Dato: - f.eks. 15-10-2022"
-          name="date"
-          value={date}
-          onChange={handleDate}
-        />
-      </div>
-      <br /> <br /> <br />
-      <div>
-        <label id="room-attribute-title">Lokalets egenskaber</label>
+      <form id="booking-modal">
+        <div>
+          <label class="label-title-styling">Lokaletype:</label>
+          <div class="btn-group">
+            <button id="active-roomtype">Undervisningslokaler</button>
+            <button id="inactive-roomtype">Mødelokaler</button>
+          </div>
+        </div>
         <br />
-        <button class="room-attributes room-attributes-inactive">32 kvm</button>
-        <button class="room-attributes room-attributes-active">24 sidderpladser</button>
-        <button class="room-attributes room-attributes-inactive">Projektor: <Check size={20}/></button>
-        <button class="room-attributes room-attributes-active">Tavle: <Check size={20}/></button>
-      </div>
-      <button onClick={handleSubmit} id="header-booking-button">Gem booking</button>
-    </form>
-  </div>
+        <div>
+          <label class="label-title-styling">Lokale:</label>
+          <input class="input-styling"
+            type="text"
+            placeholder="Lokale nummer:"
+            name="room"
+            value={room}
+            onChange={handleRoom} />
+        </div>
+        <br />
+        <div>
+          <label class="label-title-styling">Dato:</label>
+          <input class="input-styling"
+            type="text"
+            placeholder="Dato: - f.eks. 15-10-2022"
+            name="date"
+            value={date}
+            onChange={handleDate}
+          />
+        </div>
+        <br /> <br /> <br />
+        <div>
+          <label id="room-attribute-title">Lokalets egenskaber</label>
+          <br />
+          <button class="room-attributes room-attributes-inactive">32 kvm</button>
+          <button class="room-attributes room-attributes-active">24 sidderpladser</button>
+          <button class="room-attributes room-attributes-inactive">Projektor: <Check size={20} /></button>
+          <button class="room-attributes room-attributes-active">Tavle: <Check size={20} /></button>
+        </div>
+        <button onClick={handleSubmit} id="header-booking-button">Gem booking</button>
+      </form>
+    </div>
   );
 }
