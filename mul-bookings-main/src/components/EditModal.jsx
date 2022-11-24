b6595b1e7f0583f41eaa73b269e9232eacc2749c
PatrickBDJ <dyva@live.dk>
2022-11-24 12:17:19 +0100

    .

--- mul-bookings-main/src/components/EditModal.jsx
+++ mul-bookings-main/src/components/EditModal.jsx
@@ -1,22 +1,34 @@
 import {useRef} from 'react';
+import { useState } from "react";
+import { Check } from 'lucide-react';
 
 
-
-
-
-
-function EditModal(date, room, id, setBookings, bookings){
+function EditModal( id, bookings, setBookings){
     const inputRef1 = useRef(null);
     const inputRef2 = useRef(null);
 
-    const handleEdit = async (e) => {
+    const [room, setRoom] = useState("");
+    const [date, setDate] = useState(""); // date
+  
+    const handleRoom = (event) => {
+      const input = event.target.value;
+      setRoom(input);
+    };
+    const handleDate = (event) => {
+      // date
+      setDate(event.target.value);
+    };
+
+    const handleDelete = async (e) => {
       const response = await fetch(`https://react-intro-a3485-default-rtdb.europe-west1.firebasedatabase.app/${id}/.json`,
       {
-        method: 'Edit', 
+        method: 'PUT', 
       })
       const newBookings = bookings.filter(el => el.id != id);
       setBookings(newBookings);
     };
+
+    console.log(id);
 
     return(
         <div className="form-div">
@@ -28,8 +40,8 @@ function EditModal(date, room, id, setBookings, bookings){
               type="text"
               placeholder="Lokale nummer:"
               name="room"
-              /* value={room}
-              onChange={handleRoom}  */ ref={inputRef1}/>
+              value={room}
+              onChange={handleRoom}  ref={inputRef1}/>
             <button type="button" id="select" onClick={() => {inputRef1.current.focus()}}>Vælg</button>
           </div>
           <br />
@@ -39,12 +51,12 @@ function EditModal(date, room, id, setBookings, bookings){
               type="text"
               placeholder="Dato: - f.eks. 15-10-2022"
               name="date"
-             /*  value={date}
-              onChange={handleDate} */ ref={inputRef2}/>
+              value={date}
+              onChange={handleDate} ref={inputRef2}/>
             <button type="button" id="select" onClick={() => {inputRef2.current.focus()}} >Vælg</button>
           </div>
           <br /> <br /> <br /> <br />
-          <button id="save-button">Gem booking</button>
+          <button type="button" id="save-button" onClick={handleDelete}>Gem booking</button>
         </form>
       </div>
     );
