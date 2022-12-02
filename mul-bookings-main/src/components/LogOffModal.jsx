9560815ba4d9308748bee165967da44472aef373
PatrickBDJ <dyva@live.dk>
2022-12-02 13:53:47 +0100

    logo i log ud

--- mul-bookings-main/src/components/LogOffModal.jsx
+++ mul-bookings-main/src/components/LogOffModal.jsx
@@ -10,7 +10,7 @@ export default function LogOffModal() {
         <div>
             <div className="logo-login">
                 <img src={cphlogowhite} alt="Logo" id="login-styling" />
-                <h1>Lokalebookning</h1>
+                {/* <h1>Lokalebookning</h1> */}
             </div>
             <div id="log-off-text">
                 <h2>Du er nu logget ud</h2>
