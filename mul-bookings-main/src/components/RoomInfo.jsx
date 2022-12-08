1770147965c70c94ecbc7ce767594b163626c750
PatrickBDJ <dyva@live.dk>
2022-12-08 11:36:01 +0100

    tavle vs. projektor

--- mul-bookings-main/src/components/RoomInfo.jsx
+++ mul-bookings-main/src/components/RoomInfo.jsx
@@ -1,6 +1,6 @@
 import calender from "../assets/calender.svg";
 
-export default function RoomInfo({ date, room, kvm, seating, projector, blackboard }) {
+export default function RoomInfo({ date, room, kvm, seating, blackboard, projector }) {
     /*     console.log(projector); */
     return (
         <div className="Room-info">
@@ -13,8 +13,8 @@ export default function RoomInfo({ date, room, kvm, seating, projector, blackboa
                 <p>Lokaletype: Undervisningslokale</p>
                 <p>Kvadratmeter: {kvm}</p>
                 <p>Siddepladser: {seating}</p>
-                <p>Projektor: {projector == true ? "Ja" : "Nej"}</p>
                 <p>Tavle: {blackboard == true ? "Ja" : "Nej"}</p>
+                <p>Projektor: {projector == true ? "Ja" : "Nej"}</p>
             </div>
         </div>
     );
